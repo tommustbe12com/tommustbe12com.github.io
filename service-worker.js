a28720dff930033fca2757676f42c259ab74d1a4
tommustbe12com <155601372+tommustbe12com@users.noreply.github.com>
2024-06-27 20:05:23 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,38 @@
-// Service Worker Installation
-// self.addEventListener('install', function(event) {
-//   event.waitUntil(
-//     caches.open('my-cache').then(function(cache) {
-//       return cache.addAll([
-//         '/', // Add the URLs you want to cache here
-//         '/index.html',
-//         '/style.css',
-//         '/script.js',
-//         '/assets/profile.png',
-//         // Add more URLs as needed
-//       ]);
-//     })
-//   );
-// });
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll([
+        '/', // Add the URLs you want to cache here
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/assets/profile.png',
+        // Add more URLs as needed
+      ]);
+    })
+  );
+});
 
-// // Service Worker Activation
-// self.addEventListener('activate', function(event) {
-//   event.waitUntil(
-//     caches.keys().then(function(cacheNames) {
-//       return Promise.all(
-//         cacheNames.filter(function(cacheName) {
-//           // Remove any old caches here if needed
-//         }).map(function(cacheName) {
-//           return caches.delete(cacheName);
-//         })
-//       );
-//     })
-//   );
-// });
+// Service Worker Activation
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          // Remove any old caches here if needed
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
 
-// // Service Worker Fetch
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request).then(function(response) {
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
+// Service Worker Fetch
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
