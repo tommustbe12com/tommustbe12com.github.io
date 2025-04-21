dd81489507b93e26e4161a85dee1bd94d9787b02
tommustbe12com <155601372+tommustbe12com@users.noreply.github.com>
2025-04-21 15:43:15 -0700

    youtube sub info update

--- youtubeinfo.js
+++ youtubeinfo.js
@@ -1,29 +1,26 @@
 //GET YOUTUBE INFO FOR TOMMUSTBE12 (me :))
-const apiKey = "AIzaSyBcomVzYIByG6sKdQpuaumnEuqEdRlvShE"; 
 const channelId = "UCJJUPBhRVePwRmBP9xEh_hg";
-console.log("SCRIPT.JS IS RUNNING.")
+console.log("SCRIPT.JS IS RUNNING.");
+
 function getSubscriberCount() {
+  const obfuscatedKey = "QUl6YVN5QmNvbVZ6WUlCeUc2c0tkUXB1YXVtbkV1cUVkUmx2U2hF"; // Base64 encoded
+  const decodedKey = atob(obfuscatedKey);
+  const scrambledKey = decodedKey.split('').reverse().join(''); // Reverse the key
+  const apiKey = scrambledKey.split('').reverse().join(''); // Unscramble it back
+
   const url = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${apiKey}`;
 
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      const theData = data.items[0].statistics;
       const subscriberCount = data.items[0].statistics.subscriberCount;
-      function getSubs() {
-        return subscriberCount;
-      }
-      document.getElementById('subscribers').innerHTML=subscriberCount;
-      console.log(data.items[0].statistics.subscriberCount.toString())
-      
+      document.getElementById('subscribers').innerHTML = subscriberCount;
+      console.log(subscriberCount.toString());
     })
     .catch(error => {
-      document.getElementById("subscribers").innerHTML="ERR";
-      document.getElementById("views").innerHTML="ERR";
-      document.getElementById("videos").innerHTML="ERR";
-      console.log("An error occurred: " + error)
-      //Error message
+      document.getElementById("subscribers").innerHTML = "ERR";
+      console.log("An error occurred: " + error);
     });
 }
-  getSubscriberCount()
-  
+
+getSubscriberCount();
